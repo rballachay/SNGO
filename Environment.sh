bf83a5eb656969441209f761439c48fc9cd3c720
Riley Ballachay <riley.ballachay@gmail.com>
2020-06-30 16:22:19 -0600

    First Commit

---
+++ Environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  Environment.sh
+#  
+#
+#  Created by Riley Ballachay on 2020-06-11.
+#  
+
+ENV["GUROBI_HOME"] = "/Library/gurobi902/mac64"
+ENV["GRB_LICENSE_FILE"] = "/SNGO/gurobi.lic"
+ENV["SINGODIR"] = "/SNGO"
